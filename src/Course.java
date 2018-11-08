a673673b6a01520bf70a5e3c5cc27aa17144cb73
Jason <c170127@e.ntu.edu.sg>
2018-11-08 15:39:36 +0800

    add method for printing menu

--- src/Course.java
+++ src/Course.java
@@ -1,60 +1,84 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Course {
+public class Course
+{
 	private String courseName;
 	private String courseID;
 	private ArrayList<String> matNos;
 	private ArrayList<CourseWork[]> marks;
 	private ArrayList<Lesson> lessons;
-	
-	public Course(String courseID, String courseName, ArrayList<String> matNos, ArrayList<CourseWork[]> marks, ArrayList<Lesson> lessons) {
+
+	public Course(String courseID, String courseName, ArrayList<String> matNos, ArrayList<CourseWork[]> marks,
+			ArrayList<Lesson> lessons)
+	{
 		this.courseName = courseName;
 		this.courseID = courseID;
 		this.matNos = matNos;
 		this.marks = marks;
 		this.lessons = lessons;
 	}
-	public Course(String courseName, String courseID) {
+
+	public Course(String courseName, String courseID)
+	{
 		this.courseName = courseName;
-		this.courseID = courseID;	
+		this.courseID = courseID;
 	}
-	public String getCourse() {
+
+	public String getCourse()
+	{
 		return courseName;
 	}
-	public String getCourseID() {
+
+	public String getCourseID()
+	{
 		return courseID;
 	}
-	public ArrayList<String> getmatNos(){
+
+	public ArrayList<String> getmatNos()
+	{
 		return matNos;
 	}
-	public ArrayList<CourseWork[]> getMarks(){
+
+	public ArrayList<CourseWork[]> getMarks()
+	{
 		return marks;
 	}
-	public ArrayList<Lesson> getLessons(){
+
+	public ArrayList<Lesson> getLessons()
+	{
 		return lessons;
 	}
-	
-	public int searchIndex(String Index, ArrayList<String> lecIndex) {
-		for (int i = 0; i < lecIndex.size(); i++) {
-			if (lecIndex.get(i).equals(Index)) {
+
+	public int searchIndex(String Index, ArrayList<String> lecIndex)
+	{
+		for (int i = 0; i < lecIndex.size(); i++)
+		{
+			if (lecIndex.get(i).equals(Index))
+			{
 				return i;
 			}
 		}
 		return -1;
 	}
-	public void addIndex(ArrayList<String> lecIndex) {
+
+	public void addIndex(ArrayList<String> lecIndex)
+	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Add an index for lecture:");
 		String index = sc.next();
 		lecIndex.add(index);
 		sc.close();
 	}
-	public void printList(String Index, ArrayList<String> arrIndex, ArrayList<ArrayList<String>> arrStudentList) {
-		int i = searchIndex(Index,arrIndex);
-		if (i >= 0) {
+
+	public void printList(String Index, ArrayList<String> arrIndex, ArrayList<ArrayList<String>> arrStudentList)
+	{
+		int i = searchIndex(Index, arrIndex);
+		if (i >= 0)
+		{
 			ArrayList<String> studentList = arrStudentList.get(i);
-			for (int j = 0; j < studentList.size(); j++) {
+			for (int j = 0; j < studentList.size(); j++)
+			{
 				System.out.println(studentList.get(j));
 			}
 		}
--- src/SCRAMEApp.java
+++ src/SCRAMEApp.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SCRAMEApp {
-	public static void main(String args[]) throws IOException {
-		
-		String filename = "Students.txt";
-		ArrayList StudentAl = StudentDB.readStudents(filename);
-		// ArrayList CourseAl = CourseDB.readCourses(filename);
-		
-		System.out.println("Welcome to SCRAME");
-		System.out.println("Select an option:");
-		System.out.println("1. Add a student");
-		System.out.println("2. Add a course");
-		System.out.println("3. Register student for a course (this include registering for Tutorial/Lab classes)");
-		System.out.println("4. Check available slot in a class (vacancy in a class)");
-		System.out.println("5. Print student list by lecture, tutorial or laboratory session for a course.");		
-		System.out.println("6. Enter course assessment components weightage");
-		System.out.println("7. Enter coursework mark – inclusive of its components.");
-		System.out.println("8. Enter exam mark");
-		System.out.println("9. Print course statistics");
-		System.out.println("10. Print student transcript.");
+public class SCRAMEApp
+{
+	public static void main(String args[]) throws IOException
+	{
 
-		Scanner sc = new Scanner(System.in);
-		int choice = sc.nextInt();
+		String filename = "Students.txt";
+		ArrayList<Student> students = StudentDB.readStudents(filename);
+		// ArrayList CourseAl = CourseDB.readCourses(filename);
+
+		System.out.println("Welcome to SCRAME");
 		
-		while (choice >= 1 & choice <= 10) {
-			switch (choice) {
+		Scanner sc = new Scanner(System.in);
+		int choice = 0;
+
+		do
+		{
+			printMenu();
+			
+			do
+			{
+				System.out.println("Select an option:");
+				
+				if(sc.hasNextInt())
+					choice = sc.nextInt();
+			} while(choice < 1 || choice > 11);
+			
+			switch (choice)
+			{
 				case 1:
 					// Error check
 					System.out.println("Enter the name of the student: ");
@@ -36,21 +39,23 @@ public class SCRAMEApp {
 					Pattern p = Pattern.compile("^[ A-Za-z]+$");
 					Matcher m = p.matcher(name);
 					boolean b = m.matches();
-					if (!b) {
+					if (!b)
+					{
 						System.out.println("Error! Please enter only letters and spaces.");
 						break;
 					}
 					// Error check
 					System.out.println("Enter the student's ID: ");
 					String matriculationNumber = sc.next();
-					Student stu = new Student(name,matriculationNumber);
-					StudentAl.add(stu);
-					StudentDB.saveStudents("Students.txt",StudentAl);
-					
+					Student stu = new Student(name, matriculationNumber);
+					students.add(stu);
+					StudentDB.saveStudents("Students.txt", students);
+
 					System.out.println("List of all students:");
-					for (int i = 0; i < StudentAl.size(); i++) {
-						Student stu2 = (Student) StudentAl.get(i);
-						System.out.print(stu2.getName()); 
+					for (int i = 0; i < students.size(); i++)
+					{
+						Student stu2 = (Student) students.get(i);
+						System.out.print(stu2.getName());
 						System.out.println(" " + stu2.getID());
 					}
 					break;
@@ -61,13 +66,15 @@ public class SCRAMEApp {
 					String studentID = sc.next();
 					System.out.println("Enter courseID to be added: ");
 					String courseID = sc.next();
-					for (int i = 0; i < StudentAl.size(); i++) {
-						Student stu3 = (Student) StudentAl.get(i);
-						if (studentID.equals(stu3.getID())) {
+					for (int i = 0; i < students.size(); i++)
+					{
+						Student stu3 = (Student) students.get(i);
+						if (studentID.equals(stu3.getID()))
+						{
 							stu3.addCourse(courseID);
-							StudentDB.saveStudents("Students.txt",StudentAl);
+							StudentDB.saveStudents("Students.txt", students);
 							System.out.println("Course added!");
-							
+
 							break;
 						}
 					}
@@ -75,16 +82,32 @@ public class SCRAMEApp {
 				case 5:
 				case 6:
 				case 7:
-				case 8: 
-				case 9: 
+				case 8:
+				case 9:
 				case 10:
-				default: 
-						break;
-				}
-			System.out.println("Select an option:");
-			choice = sc.nextInt();
-		}
+				default:
+					break;
+			}
+			
+
+		} while (choice != 11);
+		
+		sc.close();
+	}
+	
+	public static void printMenu()
+	{
+		System.out.println(	"Select an option:\n" +
+							"1.  Add a student\n" +
+							"2.  Add a course\n" + 
+							"3.  Register student for a course (this include registering for Tutorial/Lab classes)\n" + 
+							"4.  Check available slot in a class (vacancy in a class)\n" + 
+							"5.  Print student list by lecture, tutorial or laboratory session for a course.\n" + 
+							"6.  Enter course assessment components weightage\n" + 
+							"7.  Enter coursework mark – inclusive of its components.\n" + 
+							"8.  Enter exam mark\n" + 
+							"9.  Print course statistics\n" + 
+							"10. Print student transcript.\n" +
+							"11. Quit");
 	}
 }
-
-
